fc7795a6c7e6d94708e339400b36b18a265a6357
Nika <122999874+Phutko13@users.noreply.github.com>
2023-01-18 20:04:21 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,545 @@
+let Questions = [
+  // creating Questions
+  {
+    //0
+    question:
+      "№1 When you are in a community (at a party with fellow workers or fellow practitioners), you usually prefer",
+    answerOne: "A) participate in a general conversation or",
+    answerSecond: "B) talking with each separately",
+  },
+  {
+    //1
+    question: "№2 You consider yourself more ",
+    answerOne: "A) realistic or",
+    answerSecond: "B) prone to theorizing",
+  },
+  {
+    //2
+    question: "№3 In your opinion, which one is worse?",
+    answerOne: "A) “living in a dream world”» or",
+    answerSecond: "B) «all goes according to the plan»",
+  },
+  {
+    //3
+    question: "№4 You are more impressed by",
+    answerOne: "A) solid principles or",
+    answerSecond: "B) strong emotions",
+  },
+  {
+    //4
+    question: "№5 You are more attracted  ",
+    answerOne: "A) persuasive or  ",
+    answerSecond: "B) sentimental ",
+  },
+  {
+    //5
+    question:
+      "№6 If you have to do an unusual job, it’s more convenient for you. ",
+    answerOne: "A) to plan it in advance or -",
+    answerSecond: "B) to find out what to do, already in the course of work ",
+  },
+  {
+    //6
+    question:
+      "№7 When you need to make a choice or make a decision, you make it. ",
+    answerOne: "A) mostly attentive and carefully or ",
+    answerSecond: "B) often spontaneously",
+  },
+  {
+    //7
+    question: "№8 At parties or social gatherings you usually ",
+    answerOne: "A) stay up late",
+    answerSecond: "B) leave early feeling tired",
+  },
+  {
+    //8
+    question: "№9 You are more attracted to",
+    answerOne: "A) realists or",
+    answerSecond: "B) imaginative people",
+  },
+  {
+    //9
+    question: "№10 You are more interested in",
+    answerOne: "A) what really exists or",
+    answerSecond: "B) unrealized opportunities",
+  },
+  {
+    //10
+    question: "№11 Your conclusions about people based ",
+    answerOne: "A) on rules more often than on circumstances or",
+    answerSecond: "B) on circumstances more often than on rules",
+  },
+  {
+    //11
+    question: "№12 When assessing other people, you usually",
+    answerOne: "A) firm and objective",
+    answerSecond: "B) condescending and subjective",
+  },
+  {
+    //12
+    question: "№13 More often you act",
+    answerOne: "A) punctually or",
+    answerSecond: "B) slowly",
+  },
+  {
+    //13
+    question: "№14 You prefer",
+    answerOne: "A) doing work in advance or",
+    answerSecond: "B) doing everything last moment",
+  },
+  {
+    //14
+    question: "№15 Among your friends you",
+    answerOne: "A) you know everything about everyone or",
+    answerSecond: "B) the last to know what is happening",
+  },
+  {
+    //15
+    question: "№16 When doing your usual work you prefer",
+    answerOne: "A) doing it the usual way or",
+    answerSecond: "B) inventing your own way",
+  },
+  {
+    //16
+    question: "№17 When you read at your pleasure, you prefeer when a writer",
+    answerOne: "A) clearly explains what he means or",
+    answerSecond: "B) sets out thoughts in an unusual, original form.",
+  },
+  {
+    //17
+    question: "№18 What attracts you more",
+    answerOne: "A) consistency and reasoning or",
+    answerSecond: "B) the harmony of human relations",
+  },
+  {
+    //18
+    question: "№19 It is easier for you to make judgments.",
+    answerOne: "A) based on logic and on facts or",
+    answerSecond: "B) based on your values",
+  },
+  {
+    //19
+    question: "№20 You are more attracted to situations",
+    answerOne: "A) in which there is an element of certainty",
+    answerSecond: "B) which are full of unpredictability",
+  },
+  {
+    //20
+    question: "№21 You can say that you are more ",
+    answerOne: "A) serious and focused",
+    answerSecond: "B) not taking life too seriously",
+  },
+  {
+    //21
+    question: "№22 When talking on the phone",
+    answerOne: "A) you rarely think about: “What to say?” in advance Or",
+    answerSecond: "B) often think out in advance what you will say",
+  },
+  {
+    //22
+    question: "№23 How do you think? Data",
+    answerOne: "A) 'speak for itself' or", //"speak for itself" or
+    answerSecond: "B) illustrates some patterns",
+  },
+  {
+    //23
+    question: "№24 Dreamers",
+    answerOne: "A) annoy you or",
+    answerSecond: "B) attract and fascinate you",
+  },
+  {
+    //24
+    question: "№25 More often you are",
+    answerOne: "A) calm and impartial or",
+    answerSecond: "B) cordial and sympathetic",
+  },
+  {
+    //25
+    question: "№26 You think that it is worse to",
+    answerOne: "A) be unreasonable or",
+    answerSecond: "B) be malevolent",
+  },
+  {
+    //26
+    question: "№27 In most cases it is better to",
+    answerOne: "A) try to control the course of events or",
+    answerSecond: "B) rely on the natural course of events.",
+  },
+  {
+    //27
+    question: "№28 You feel better when",
+    answerOne: "A) you have already made a purchase or",
+    answerSecond: "B) you still have the opportunity to do it.",
+  },
+  {
+    //28
+    question: "№29 In the group of colleagues you tend to",
+    answerOne: "A) initiate a conversation or",
+    answerSecond: "B) wait for someone to contact you",
+  },
+  {
+    //29
+    question: "№30 Claims made on the basis of common sense",
+    answerOne: "A) rarely raise doubts or",
+    answerSecond: "B) often raise doubts.",
+  },
+  {
+    //30
+    question: "№31 Would you like your friend to be a person",
+    answerOne: "A) who 'stands firmly on the ground' or", //who "stands firmly on the ground" or
+    answerSecond: "B) who always has new ideas.",
+  },
+  {
+    //31
+    question: "№32 When making decisions it is easier for you to follow",
+    answerOne: "A) the rules and standards or",
+    answerSecond: "B) the wishes of interested parties",
+  },
+  {
+    //32
+    question: "№33 You are rather a person",
+    answerOne: "A) hard than soft or",
+    answerSecond: "B) soft than hard",
+  },
+  {
+    //33
+    question: "№34 You are more attracted by the opportunity",
+    answerOne: "A) to organize something or",
+    answerSecond: "B) creatively use the available opportunities",
+  },
+  {
+    //34
+    question: "№35 In a situation you value",
+    answerOne: "A) clarity or",
+    answerSecond: "B) the possibility of unpredictable turns.",
+  },
+  {
+    //35
+    question: "№36 Meeting new and unusual people.",
+    answerOne: "A) stimulates and energizes you or",
+    answerSecond: "B) tires you",
+  },
+  {
+    //36
+    question: "№37 In most cases you are",
+    answerOne: "A) practical or",
+    answerSecond: "B) a dreamer ",
+  },
+  {
+    //37
+    question: "№38 You are more interested to know",
+    answerOne: "A) wheather other people mayt be helpful to you or",
+    answerSecond: "B) other people's point of view",
+  },
+  {
+    //38
+    question: "№39 You receive more satisfaction ",
+    answerOne: "A) through a discussion of the issue or",
+    answerSecond: "B) through reaching agreement on the subject matter.",
+  },
+  {
+    //39
+    question: "№40 Your actions are more governed by",
+    answerOne: "A) your head or",
+    answerSecond: "B) your heart",
+  },
+  {
+    //40
+    question:
+      "№41 When you know that at a certain time you will be engaged in a certain business,",
+    answerOne: "A) you are glad that you can plan your time or",
+    answerSecond: "B) you don't like when you have to follow a certain plan",
+  },
+  {
+    //41
+    question: "№42 Usually you",
+    answerOne: "A) strive to achieve desired results or",
+    answerSecond: "B) are satisfied with what comes out",
+  },
+  {
+    //42
+    question: "№43 Do you prefer to have",
+    answerOne:
+      "A) a wide circle of communication, consisting of many friends and acquaintances or",
+    answerSecond: "B) a narrow social circle consisting of several friends",
+  },
+  {
+    //43
+    question: "№44 You are more guided by",
+    answerOne: "A) facts or",
+    answerSecond: "B) regularities",
+  },
+  {
+    //44
+    question: "№45 You are more interested or would be interested in",
+    answerOne: "A) production and distribution of products or",
+    answerSecond: "B) research and design",
+  },
+  {
+    //45
+    question: "№46 Do you consider it a compliment if you are called",
+    answerOne: "A) a logical person or",
+    answerSecond: "B) the feeling person",
+  },
+  {
+    //46
+    question: "№47 In yourself you value more",
+    answerOne: "A) decisiveness or",
+    answerSecond: "B) devotion",
+  },
+  {
+    //47
+    question:
+      "№48 When you express your thoughts or judgments, you prefer them to be",
+    answerOne: "A) completed or",
+    answerSecond: "B) preliminary with the possibility of improvement",
+  },
+  {
+    //48
+    question: "№49 You feel better",
+    answerOne: "A) after making an important decision or",
+    answerSecond: "B) before making an important decision.",
+  },
+  {
+    //49
+    question: "№50 When communicating with unfamiliar people, you",
+    answerOne: "A) have long conversations or",
+    answerSecond: "B) hardly find topics for conversation",
+  },
+  {
+    //50
+    question: "№51 You trust ",
+    answerOne: "A) your experience or",
+    answerSecond: "B) your intuition",
+  },
+  {
+    //51
+    question: "№52 Would you prefer to be considered as",
+    answerOne: "A) a practical person or",
+    answerSecond: "B) an inventive person",
+  },
+  {
+    //52
+    question: "№53 More praiseworthy is a man",
+    answerOne: "A) who has “clear mind” or",
+    answerSecond: "B) capable of strong feelings",
+  },
+  {
+    //53
+    question: "№54 You are more likely to be",
+    answerOne: "A) fair and impartial or ",
+    answerSecond: "B) sympathetic and empathizing",
+  },
+  {
+    //54
+    question: "№55 If you act according to a plan",
+    answerOne: "A) you feel comfortable or ",
+    answerSecond: "B) it constrains and limits you ",
+  },
+  {
+    //55
+    question: "№56 You do things better when you are",
+    answerOne: "A) following a well-thought-out plan or ",
+    answerSecond: "B) coping with surprises",
+  },
+  {
+    //56
+    question:
+      "№57 Do you think that people close to you have your opinion on certain issues?",
+    answerOne: "A) know your opinion on certain issues, or",
+    answerSecond: "B) will find out only when you tell them .",
+  },
+  {
+    //57
+    question: "№58 In yourself you value",
+    answerOne: "A) a strong sense of reality or",
+    answerSecond: "B) a vivid imagination",
+  },
+  {
+    //58
+    question: "№59 If you were a teacher, you would prefer to teach ",
+    answerOne: "A) practical subjects or",
+    answerSecond: "B) theoretical subjects ",
+  },
+  {
+    //59
+    question: "№60 what do you think is worse - to be",
+    answerOne: "A) too quick-tempered or ",
+    answerSecond: "B) too objective",
+  },
+  {
+    //60
+    question: "№61 You consider yourself as",
+    answerOne: "A) sober-minded or ",
+    answerSecond: "B) loyal and responsive ",
+  },
+  {
+    //61
+    question: "№62 You are more attracted to situations",
+    answerOne: "A) ordered and planned",
+    answerSecond: "B) unordered and unplanned",
+  },
+  {
+    //62
+    question: "№63 You are acting ",
+    answerOne:
+      "A) more often in accordance to the rules rather than the way you actually want or ",
+    answerSecond:
+      "B) more often the way you actually want rather than in accordance to the rules",
+  },
+  {
+    //63
+    question: "№64 Usually you are",
+    answerOne: "A) a sociable person or ",
+    answerSecond: "B) calm and discreet",
+  },
+  {
+    //64
+    question: "№65 When you are writing, you prefer",
+    answerOne: "A) writing word for word or",
+    answerSecond: "B) expressing  figuratively.",
+  },
+  {
+    //65
+    question: "№66 You find it harder",
+    answerOne: "A) to put yourself in the shoes of other people or",
+    answerSecond:
+      "B) to understand how to get the most benefit from other people.",
+  },
+  {
+    //66
+    question: "№67 Would you wish yourself",
+    answerOne: "A) a greater mental clarity or",
+    answerSecond: "B) a greater compassion ability",
+  },
+  {
+    //67
+    question: "№68 Would you prefer working with a manager who",
+    answerOne: "A) is always fair or",
+    answerSecond: "B) is always benevolent.",
+  },
+  {
+    //68
+    question: "№69 Do you prefer",
+    answerOne: "A) scheduled events or",
+    answerSecond: "B) spontaneous events",
+  },
+  {
+    //69
+    question: "№70 You are more inclined",
+    answerOne: "A) to think and plan or",
+    answerSecond: "B) to act according to circumstances",
+  },
+];
+let count = 0; //first page
+function nextQuestion(questionId, firstAnswer, secondAnswer) {
+  count++;
+  let question = Questions[count].question;
+  let answerOne = Questions[count].answerOne;
+  let answerSecond = Questions[count].answerSecond;
+  document.getElementById(`${questionId}`).innerHTML = `${question}`;
+  document.getElementById(`${firstAnswer}`).innerHTML = `${answerOne}`;
+  document.getElementById(`${secondAnswer}`).innerHTML = `${answerSecond}`;
+  // if (count % 2 == 0) {
+  //     document.getElementById(`displayQuestion`).style.display = "flex"
+  // } else {
+  //     document.getElementById(`displayQuestion`).style.display = "block"
+  // }
+  sumbitBox();
+}
+
+function beforeQuestion(questionId, firstAnswer, secondAnswer) {
+  if (count == 0) return alert("Can't go more backward");
+  count--;
+  let question = Questions[count].question;
+  let answerOne = Questions[count].answerOne;
+  let answerSecond = Questions[count].answerSecond;
+  document.getElementById(`${questionId}`).innerHTML = `${question}`;
+  document.getElementById(`${firstAnswer}`).innerHTML = `${answerOne}`;
+  document.getElementById(`${secondAnswer}`).innerHTML = `${answerSecond}`;
+}
+
+function getQuestion(questionId, firstAnswer, secondAnswer, goingQuestion) {
+  count = goingQuestion;
+  let question = Questions[count].question;
+  let answerOne = Questions[count].answerOne;
+  let answerSecond = Questions[count].answerSecond;
+  document.getElementById(`${questionId}`).innerHTML = `${question}`;
+  document.getElementById(`${firstAnswer}`).innerHTML = `${answerOne}`;
+  document.getElementById(`${secondAnswer}`).innerHTML = `${answerSecond}`;
+}
+
+function clearCheckBox(check) {
+  let checkBoxes = document.getElementsByName(`${check}`);
+  checkBoxes.forEach((element) => {
+    element.checked = false;
+  });
+  count = 0;
+  sumbitedBoxs = 0;
+  for (let i = 0; i < maxQuestion; i++) {
+    document.getElementById(`${i}`).style.background = "";
+  }
+  getQuestion("questionHolder", "firstAnswer", "secondAnswer", `0`);
+}
+let presCount = 1;
+
+function pressCount() {
+  presCount++;
+}
+
+function checkAllBox(count) {
+  if (count == maxQuestion) {
+    document.getElementById(`displayQuestion`).style.display = "none";
+    document.getElementById(`finish`).innerHTML = "თქვენ დაამთავრეთ ტესტი";
+  }
+}
+let sumbitedBoxs = 0;
+
+function sumbitBox() {
+  maxQuestion = 70;
+  let id = count;
+  let checkOne = document.getElementById("AnswerInputFirst").checked;
+  let chekcSecond = document.getElementById("AnswerInputSecond").checked;
+  if (checkOne || chekcSecond) {
+    if (presCount > 1) {
+      document.getElementById(`${id - 1}`).style.background = "green";
+      console.log(`${id}`);
+      sumbitedBoxs++;
+      checkAllBox(sumbitedBoxs);
+      //          if (sumbitedBoxs != maxQuestion) {
+      //               nextQuestion("questionHolder", "firstAnswer", "secondAnswer");
+      //           }
+    }
+    let checkBoxes = document.getElementsByName(`check`);
+    checkBoxes.forEach((element) => {
+      element.checked = false;
+    });
+  }
+  switchDesgin();
+}
+
+function finish() {
+  if (sumbitedBoxs != 5) return alert("You must answer 70 questions!");
+}
+
+let desingOption = 1;
+function switchDesgin() {
+  if (desingOption == 0) {
+    defaultDesign();
+    desingOption++;
+    return;
+  }
+  if (desingOption == 1) {
+    anotherDesign();
+    desingOption--;
+    return;
+  }
+}
+function defaultDesign() {
+  document.getElementById(`firstContainer`).style.width = "100%";
+  document.getElementById(`secondContainer`).style.width = "100%";
+}
+function anotherDesign() {
+  document.getElementById(`firstContainer`).style.width = "auto";
+  document.getElementById(`secondContainer`).style.width = "auto";
+}
